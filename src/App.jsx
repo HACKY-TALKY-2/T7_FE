f9b0cb99e987b46d707bb493d71ac6dcb123a4ae
yalu6809 <shinwoo0226@gmail.com>
2023-11-10 21:24:58 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>ㅎㄹ</div>
+      <Button>안녕하세요</Button>
+    </>
+  );
+}
+
+export default App;
